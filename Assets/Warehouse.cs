b63acbe2cf27ca1711e340c3fa154d0586a0103f
AzThunder <just.bescop@outlook.fr>
2022-01-05 15:36:40 +0100

    package et destination

--- Assets/Warehouse.cs
+++ Assets/Warehouse.cs
@@ -6,8 +6,14 @@ using UnityEngine.UI;
 public class Warehouse : MonoBehaviour
 {
     public GameObject agentPrefab;
+    public GameObject packagePrefab;
+    public GameObject destinationPrefab;
     public int nbAgents;
+    public int nbPackages;
+    public int nbDestinations;
     public List<Agent> agents;
+    public List<Package> packages;
+    public List<Destination> destinations;
     public int width = 10;
     public int height = 10;
     public float dt = 0.002f;
@@ -17,11 +23,25 @@ public class Warehouse : MonoBehaviour
     void Start()
     {
         agents = new List<Agent>();
+        packages = new List<Package>();
+        destinations = new List<Destination>();
         for(int i=0; i<nbAgents;i++)
         {
             GameObject a = Instantiate(agentPrefab,new Vector3(Random.Range((int)-width/2,(int)(width/2)+1),Random.Range((int)-height/2,(int)(height/2)+1),0),Quaternion.identity);
             a.GetComponent<Agent>().setWitdhHeightWareHouse(width,height); //On donne l'info sur la longueur et la hauteur de la warehouse
             agents.Add(a.GetComponent<Agent>());
+        }
+
+        for(int i=0; i < nbPackages; i++)
+        {
+            GameObject p = Instantiate(packagePrefab, new Vector3(Random.Range((int)-width / 2, (int)(width / 2) + 1), Random.Range((int)-height / 2, (int)(height / 2) + 1), 0), Quaternion.identity);
+            packages.Add(p.GetComponent<Package>());
+        }
+
+        for (int i = 0; i < nbDestinations; i++)
+        {
+            GameObject p = Instantiate(destinationPrefab, new Vector3(Random.Range((int)-width / 2, (int)(width / 2) + 1), Random.Range((int)-height / 2, (int)(height / 2) + 1), 0), Quaternion.identity);
+            destinations.Add(p.GetComponent<Destination>());
         }
         prevdt = dt;
     }
